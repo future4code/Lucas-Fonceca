718abc05ab503e57c6d716b63968db9a09b678f3
Lucas Txai <txaifonceca@gmail.com>
2021-09-10 10:00:38 -0300

    Resolvendo os exercícios da aula 4, quinzena 1 em index.js

---
+++ quinzena1/aula4/index.js
@@ -0,0 +1,71 @@
+//EXERCÍCIOS
+
+//1
+
+// let numeroUm = 1
+// let numeroDois = 2
+// let numeroTres = 3
+// let numeroQuatro = 4
+// let numeroCinco = 5
+// let numeroNegativo = -1
+// let duzentosTrintaQuatro = 234
+
+
+// let exercicioUm = numeroTres + numeroQuatro
+// let exercicioDois = (numeroTres * numeroCinco) / numeroDois
+// let exercicioTres = (numeroQuatro - numeroCinco) * numeroNegativo
+// let exercicioQuatro = duzentosTrintaQuatro % numeroCinco
+
+// console.log (exercicioUm)
+// console.log (exercicioDois)
+// console.log (exercicioTres)
+// console.log (exercicioQuatro)
+
+// EXERCÍCIO 2
+
+// let a = 13
+// let b = 1
+
+// console.log("O primeiro número é igual ao segundo?", a === b) //1
+// console.log("O primeiro número é diferente do segundo?", a !== b) //2
+// console.log("O primeiro número é maior que o segundo?", a > b) //3
+// console.log ("O primeiro número é menor que o segundo?", a < b) //4
+
+
+// EXERCÍCIO 3
+
+// let a = true
+// let b = false
+// let c = true
+
+// console.log (a && b) // 1 false
+// console.log (b && c) // 2 false
+// console.log (a && c) // 3 true
+// console.log (a && b && c) // 4 false
+
+// EXERCÍCIO 4
+
+// let a = true
+// let b = false
+// let c = true
+
+// console.log (a || b) // true
+// console.log (b || c) // true
+// console.log (a || c) // true
+// console.log (a || b || c) // true 
+
+// EXERCÍCIO 5
+
+// let nomeUsuario = prompt("Digite seu nome");
+// let anoNascimento = Number(prompt("Digite seu ano de nascimento"));
+// let anoAtual = Number(prompt("Em que ano estamos?"));
+// let anoAleatorio = 2050;
+
+
+// let idade = anoAtual - anoNascimento
+
+// console.log ("Olá", nomeUsuario);
+// console.log ("Idade:", idade);
+// console.log ("É maior de idade?", (idade > 18));
+// console.log ("Idade em 2050:", (anoAleatorio - anoNascimento));
+
