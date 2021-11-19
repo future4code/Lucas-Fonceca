8f368b220a92b6cce068240d7d61f46aff0eacbf
Lucas Txai <txaifonceca@gmail.com>
2021-11-19 13:03:56 -0300

    tentei resolver o checkpoint 1 e 2 do exercício insta4

--- estados-insta4/insta4/src/App.js
+++ estados-insta4/insta4/src/App.js
@@ -10,25 +10,42 @@ const MainContainer = styled.div`
 `
 
 class App extends React.Component {
+
+  state = { 
+    perfis: [
+      {
+        nomeUsuario: 'anna flavia',
+        fotoUsuario: 'https://picsum.photos/50/50',
+        fotoPost: 'https://picsum.photos/200/150'
+      },
+      {
+        nomeUsuario: 'lucas txai',
+        fotoUsuario: 'https://picsum.photos/id/1005/50/50',
+        fotoPost: 'https://picsum.photos/id/1012/200/150' 
+      },
+      {
+        nomeUsuario: 'iris',
+        fotoUsuario: 'https://picsum.photos/id/1011/50/50',
+        fotoPost: 'https://picsum.photos/id/1015/200/150'  
+      }
+
+    ]
+  }
+
   render() {
+    const listaDePerfis = this.state.perfis.map((perfil) =>
+    {
+    return (
+      <div>
+        <img src={perfil.fotoUsuario} alt='foto de perfil do usuario' />
+        {perfil.nomeUsuario}
+        <img src={perfil.fotoPost} alt='foto do post' />
+      </div>
+    );
+    });
     return (
       <MainContainer>
-        <Post
-          nomeUsuario={'paulinha'}
-          fotoUsuario={'https://picsum.photos/50/50'}
-          fotoPost={'https://picsum.photos/200/150'}
-        />
-        <Post
-          nomeUsuario={'txai'}
-          fotoUsuario={'https://picsum.photos/id/1005/50/50'}
-          fotoPost={'https://picsum.photos/id/1012/200/150'}
-        />
-        <Post
-          nomeUsuario={'iris'}
-          fotoUsuario={'https://picsum.photos/id/1011/50/50'}
-          fotoPost={'https://picsum.photos/id/1015/200/150'}
-        />
-        
+        <div>{listaDePerfis}</div>
       </MainContainer>
     );
   }
