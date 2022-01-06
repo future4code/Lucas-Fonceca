ff1a13bc67efad80dbd33c139a15c3027b56d905
Lucas Txai <txaifonceca@gmail.com>
2022-01-06 12:49:41 -0300

    atividades hooks api useEffect

---
+++ aula32-use-effect/src/components/PokeCard/PokeCard.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const PokeCard = (props) => {
+  const [pokemon, setPokemon] = useState ({})
+  
+  useEffect (() => {
+    const pegaPokemon = async () => {
+      try {
+        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${props.pokemon}`)
+        setPokemon(response.data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    pegaPokemon()
+  }, [props.pokemon])
+
+  return (
+    <div>
+      <p>{pokemon.name}</p>
+      <p>{pokemon.weight} Kg</p>
+      {pokemon.types && <p>{pokemon.types[0].type.name}</p>}
+      {pokemon.sprites && (
+        <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+      )}
+    </div>
+  );
+}
+
+export default PokeCard;
+
+
+
+
+
