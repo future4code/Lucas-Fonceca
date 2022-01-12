2d60c2c9615bb65e0995558accefac447acd83bd
Lucas Txai <txaifonceca@gmail.com>
2022-01-11 23:06:24 -0300

    Arrumei a navegação do componente Home para o componente Matches

--- projeto-astromatch/src/App.js
+++ projeto-astromatch/src/App.js
@@ -1,14 +1,14 @@
 import { React, useState } from 'react'
-import { ChakraProvider, Heading, Grid, GridItem, Flex, IconButton  } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react'
 import { Home } from './Components/Home'
-import { RiChatHeartLine } from 'react-icons/ri'
+
 import { Matches } from "./Components/Matches"
 
 
 
 function App() {
 
-  const [screen, setScreen] = useState('')
+  const [screen, setScreen] = useState("home")
 
   const goToMatchesPage = () => {
     setScreen("matches")
@@ -18,50 +18,15 @@ function App() {
     setScreen("home")
   }
 
-  const changeScreen = () => {
-
-    switch (screen) {
-      case "home":
-        return <Home goToMatchesPage={goToMatchesPage}/>
-      case "matches":
-        return <Matches goToHomePage={goToHomePage}/>
-      default:
-        return <Home goToMatchesPage={goToMatchesPage}/>
-    }
-  }
-
 
   return (
     <ChakraProvider>
-      <Grid templateColumns='repeat(5, 1fr)' gap={6}>
-        <GridItem 
-        w='100%' 
-        colStart={3} 
-        bg={'lightgray'} 
-        marginTop={9} 
-        borderRadius={13}
-        boxShadow='base'
-        >
-          <Flex
-          mt='1'
-          fontWeight='bold'
-          lineHeight='tall'
-          isTruncated
-          textAlign={'left'}
-          justify={'space-around'}
-          >
-            <Heading
-            paddingBottom={2}
-            as='h5'
-            >
-            AstroMatch</Heading>
-            <IconButton as={RiChatHeartLine}  bg={'lightgray'} color={'purple.500'} onClick={changeScreen()}/>
-          </Flex>
-          {changeScreen()}
-          {/* <Home></Home> */}
-        </GridItem> 
-      </Grid>
-      
+      {screen === "home" ? 
+      <Home
+      goToMatchesPage={goToMatchesPage} 
+      /> : 
+      <Matches goToHomePage={goToHomePage} />
+      }
     </ChakraProvider>
   )
 } 
--- projeto-astromatch/src/Components/Home.js
+++ projeto-astromatch/src/Components/Home.js
@@ -1,13 +1,14 @@
-import {React, useState, useEffect, Profiler} from "react"
-import { Box, Image, IconButton, Grid } from '@chakra-ui/react'
+import {React, useState, useEffect} from "react"
+import { Box, Image, IconButton, Grid, GridItem, Heading, Flex, Button, propNames } from '@chakra-ui/react'
 import { AiFillHeart, AiOutlineClose } from 'react-icons/ai'
+import { RiChatHeartLine } from 'react-icons/ri'
+
+
 import axios  from 'axios'
 import { BASE_URL } from "../Constants/urls"
 
-export const Home = () => {
+export const Home = (props) => {
     const [person, setPerson] = useState([])
-
-  
 
     const getProfileList = () => {
       axios.get(`${BASE_URL}/person`)
@@ -19,8 +20,36 @@ export const Home = () => {
       getProfileList()
     }, [])
     return (
-      <Box maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden' bg={'lightgray'}>
-        
+      <Box maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden' bg={'lightgray'} align={'center'}>
+        <Box w='100%' 
+            colStart={3} 
+            bg={'lightgray'} 
+            marginTop={9} 
+            borderRadius={13}
+            boxShadow='base'
+            >
+              <Flex
+                mt='1'
+                fontWeight='bold'
+                lineHeight='tall'
+                isTruncated
+                textAlign={'left'}
+                justify={'space-around'}
+                >
+                  <Heading
+                  paddingBottom={2}
+                  as='h5'
+                  >
+                    AstroMatch
+                  </Heading>
+                  <Button 
+                  as={RiChatHeartLine}  
+                  bg={'lightgray'} 
+                  color={'purple.500'}
+                  onClick={props.goToMatchesPage}
+                  />
+              </Flex>
+            </Box>
         <Image src={person.photo}/>
           <Box p='6'>
           <Box display='flex' alignItems='baseline'>
@@ -30,7 +59,6 @@ export const Home = () => {
             fontWeight='semibold'
             as='h1'
             lineHeight='tight'
-            isTruncated
             textAlign={'center'}
           >
             {person.name}, {person.age}
@@ -41,7 +69,6 @@ export const Home = () => {
             fontWeight='normal'
             as='h6'
             lineHeight='tight'
-            isTruncated
             textAlign={'center'}
           >
             {person.bio}            
