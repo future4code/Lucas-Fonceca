1083dd51c34a2dd4697296309a8fa7023cbe45f7
Lucas Txai <txaifonceca@gmail.com>
2022-01-18 18:48:42 -0300

    alinhei as boxes ao centro da página.

--- modulo-react-avancado-hooks/projeto-labex/src/Pages/HomePage.js
+++ modulo-react-avancado-hooks/projeto-labex/src/Pages/HomePage.js
@@ -1,4 +1,4 @@
-import { Flex, Spacer, Stack, InputGroup, InputLeftAddon, Input, Button, Heading, Text, Center } from '@chakra-ui/react'
+import { Flex, Spacer, Stack, InputGroup, InputLeftAddon, Input, Button, Heading, Text, Center, Link } from '@chakra-ui/react'
 import { ArrowForwardIcon } from '@chakra-ui/icons'
 
 function HomePage() {
@@ -6,10 +6,13 @@ function HomePage() {
     
     return (
         <div>
-            <Flex maxW='container.xl' padding={8}>
+            <Flex maxW='container.xl' 
+            padding={8}
+            align={'center'}
+            justify={'space-evenly'}>
                 <Heading as='h1' size={'2xl'}>LabeX</Heading>
                 <Spacer />
-                <Stack spacing={4}>
+                <Stack spacing={4} justifySelf={'flex-end'}>
                     <InputGroup>
                         <InputLeftAddon children='Login:' />
                         <Input type='text' placeholder='Login' />
@@ -19,15 +22,26 @@ function HomePage() {
                         <Input type={'password'} placeholder='Senha' />
                     </InputGroup>
                 </Stack>
-                <Button rightIcon={<ArrowForwardIcon />} colorScheme='teal' variant='outline'>
+                <Button rightIcon={<ArrowForwardIcon />} colorScheme='teal' variant='outline' margin={2}>
                     Login
                 </Button>
             </Flex>
             <Center>
                 <Heading as='h2' size='lg'>Sobre a LabeX</Heading>
             </Center>
-            <Flex textAlign={'center'}>
-                <Text w={500}> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse lacinia erat et ligula commodo, non gravida elit elementum. Donec euismod mi diam, quis auctor enim pretium sit amet. Curabitur pellentesque odio eu lectus tincidunt, vel mattis nunc suscipit. Sed faucibus leo nibh, vitae sodales massa pellentesque quis. Donec semper turpis leo, quis tempor risus luctus sit amet. Phasellus facilisis bibendum elit, vel hendrerit velit tristique nec. Morbi faucibus sed felis id gravida. Nullam diam erat, rhoncus id mattis at, faucibus quis arcu.</Text>
+            <Flex justify={'center'}>
+                <Flex 
+                border={'2px'}
+                borderColor={'gray.300'}
+                w={'500px'}
+                marginTop={'4'}
+                justifyItems={'center'}>
+                    <Text w={500}
+                    padding={4}> 
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse lacinia erat et ligula commodo, non gravida elit elementum. Donec euismod mi diam, quis auctor enim pretium sit amet. Curabitur pellentesque odio eu lectus tincidunt, vel mattis nunc suscipit. Sed faucibus leo nibh, vitae sodales massa pellentesque quis. Donec semper turpis leo, quis tempor risus luctus sit amet. Phasellus facilisis bibendum elit, vel hendrerit velit tristique nec. Morbi faucibus sed felis id gravida. {' '} <br/>
+                        <Link href='#' color={'red'} textDecoration={'underline'} textAlign={'center'}>Descubra nossos destinos!</Link>
+                    </Text>
+                </Flex>
             </Flex>
         </div>
     )
