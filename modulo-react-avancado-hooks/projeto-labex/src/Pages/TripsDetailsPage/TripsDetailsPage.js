b805c052176e2fefe85ec0e1cd683ddb0357ac04
Lucas Txai <txaifonceca@gmail.com>
2022-01-26 17:15:20 -0300

    alterei o layout da página de candidatos

--- modulo-react-avancado-hooks/projeto-labex/src/Pages/TripsDetailsPage/TripsDetailsPage.js
+++ modulo-react-avancado-hooks/projeto-labex/src/Pages/TripsDetailsPage/TripsDetailsPage.js
@@ -6,13 +6,20 @@ import { BASE_URL } from "../../Utils/links"
 import { 
     Flex,
     Box,
-    Text
+    Text,
+    Grid,
+    Heading,
+    GridItem,
+    ButtonGroup,
+    Button,
+    IconButton,
+    Tooltip,
 } from "@chakra-ui/react"
+import { AddIcon } from '@chakra-ui/icons'
 
 function TripsDetailsPage() {
     
     const { id } = useParams();
-    const [tripId, setTripId] = useState();
     const [tripCandidates, setTripCandidates] = useState([]);
 
     const getTripsById = () => {
@@ -29,29 +36,68 @@ function TripsDetailsPage() {
         getTripsById()
     }, [])
 
+    let isCrewMember = false;
+
+    const onClickCrewMember = () => {
+        isCrewMember = true;
+        console.log(`É tripulante? ${isCrewMember}`)
+    }
 
     const renderedTripCandidatesList = tripCandidates.map((candidates) => {
         return(
-        <Flex>
-            <Box ml='3'>
-                <Text>
-                    <strong>
-                        {`Nome: `}
-                    </strong> 
-                        {candidates.name}
-                </Text>
-                <Text fontSize='sm'> <strong>{`Profissão: `}</strong>
-                    {candidates.profession}
-                </Text>
-            </Box>
+        <Flex
+            justify={'center'}
+            key={candidates.id}
+            >
+            <Flex 
+                border={"1px"}
+                w={'350px'}
+                marginTop={'13px'}
+                justify={'space-evenly'}
+                paddingTop={'8px'}
+                paddingBottom={'8px'}
+                >
+                <Box ml='3'>
+                    <Text>
+                        <strong>
+                            {`Nome: `}
+                        </strong> 
+                            {candidates.name}
+                    </Text>
+                    <Text fontSize='sm'> <strong>{`Profissão: `}</strong>
+                        {candidates.profession}
+                    </Text>
+                </Box>
+                <ButtonGroup size='sm' isAttached variant='outline' alignSelf={'center'}>
+                    
+                    <Tooltip label='Ver detalhes'>
+                        <Button mr='-px'>Detalhes</Button>
+                    </Tooltip>
+                    <Tooltip label='Adicionar à lista de Tripulantes'>
+                        <IconButton aria-label='Add to friends' icon={<AddIcon />} onClick={onClickCrewMember}/>
+                    </Tooltip>
+                </ButtonGroup>
+            </Flex>
         </Flex>
         )
     })
 
     return (
         <div>
-            {PagesHeader()}    
-            {renderedTripCandidatesList}
+            {PagesHeader()}
+            <Grid gridTemplateColumns={'repeat(2, 1fr)'}>
+                <GridItem align={'center'}>
+                    <Heading as='h3' size='lg'>
+                        Lista de Candidatos:
+                    </Heading>
+                    {renderedTripCandidatesList}
+                </GridItem>
+                <GridItem align={'center'}>
+                    <Heading as='h3' size='lg'>
+                        Lista de Tripulantes
+                    </Heading>
+                </GridItem>
+            </Grid>
         </div>
     )
 }
